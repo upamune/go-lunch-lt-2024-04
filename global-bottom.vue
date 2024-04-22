09a5063285b5a24becd57cefc71dae1a108e9ff8
Yu SERIZAWA <info@serizawa.me>
2024-04-22 22:03:41 +0900

    fix

--- global-bottom.vue
+++ global-bottom.vue
@@ -4,6 +4,6 @@
       v-if="$slidev.nav.currentLayout !== 'cover'"
       class="absolute bottom-0 left-0 right-0 text-gray-500 p-2 text-xs"
     >
-    © 2024 LayerX Inc.
+    © 2024 LayerX Inc.  {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
     </footer>
   </template>
